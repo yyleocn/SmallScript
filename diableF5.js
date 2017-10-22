ec81026b8c38f7e35e8219e372e389c167546615
XiaoLe <youngleocn@qq.com>
2017-10-22 12:36:41 +0800

    add disableF5

---
+++ diableF5.js
@@ -0,0 +1,11 @@
+javascript:
+document.onkeydown = function (event_) {
+    if (event_.which == 116) {
+        return false;
+    }
+    if (event.keyCode == 116) {
+        event.keycode == 0;
+        event.returnValue = false;
+        return false;
+    }
+}
